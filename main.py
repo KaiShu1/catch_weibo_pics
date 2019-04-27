aff91c17e9e04dd4d7d3d07a929dba369ef690cb
Kai <1010363833@qq.com>
2019-04-27 23:59:50 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import requests
-import json
-import time
+from requests import get
+from json import loads
+from time import sleep
+from os import mkdir
 
 
 class Spider:
     def __init__(self, origin_url, count):
         self.url = origin_url
-        self.r = requests.get(self.url).text
-        self.data = json.loads(self.r)
+        self.r = get(self.url).text
+        self.data = loads(self.r)
         self.count = count
 
     def catch_since_id(self):
@@ -15,21 +16,24 @@ class Spider:
         return since_id
 
     def catch_pics(self):
+        # 每个json数据中有十个card
         for i in range(10):
             try:
                 single_pic_url = self.data["data"]["cards"][i]["mblog"]["original_pic"]
             except KeyError:
                 continue
-            pic = requests.get(single_pic_url).content
+            pic = get(single_pic_url).content
             with open("./pics/%s%s.jpg" % (i, self.count), "wb") as f:
                 f.write(pic)
 
 
 if __name__ == '__main__':
+    mkdir('pics')
+    # 初始手机端主页xhr文件地址
     url = "https://m.weibo.cn/api/container/getIndex?uid=5404464551&luicode=10000011&lfid=100103type%3D3%26q%3D%E6\
     %9D%A8%E6%99%A8%E6%99%A8%26t%3D0&type=uid&value=5404464551&containerid=1076035404464551"
-    for i in range(10):
-        s = Spider(url, i)
+    for num in range(2):
+        s = Spider(url, num)
         s.catch_pics()
         url = url + "&since_id=" + str(s.catch_since_id())
-        time.sleep(1)
+        sleep(1)
