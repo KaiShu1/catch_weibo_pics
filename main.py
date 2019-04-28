78120887308c2b7654776d7ec5520f7f331d1a01
kai <1010363833@qq.com>
2019-04-28 14:46:36 +0800

    add function get search page pics and perfect get home page pics

--- main.py
+++ main.py
@@ -2,38 +2,70 @@ from requests import get
 from json import loads
 from time import sleep
 from os import mkdir
+from urllib.parse import quote
+
+'''
+Todo1: 更新主页和搜索页 多页爬取
+Todo2: 抓取gif图
+'''
 
 
 class Spider:
-    def __init__(self, origin_url, count):
-        self.url = origin_url
-        self.r = get(self.url).text
-        self.data = loads(self.r)
+    def __init__(self, content, count=0):
+        """
+        :param content: contain the thing that you want to search
+        :param count: don't need, just for count
+        """
+        self.content = content
         self.count = count
 
-    def catch_since_id(self):
-        since_id = self.data["data"]["cardlistInfo"]["since_id"]
-        return since_id
+    def home(self):
+        dir_name = self.content + 'pics_h'
+        mkdir(dir_name)
+        url = "https://m.weibo.cn/api/container/getIndex?uid=1831216671&luicode=10000011&lfid=100103" \
+              "type%3D1%26q%3D" + quote(self.content) + "&type=uid&value=1831216671&containerid=1076031831216671"
+        r = get(url).text
+        data = loads(r)
+        since_id = str(data["data"]["cardlistInfo"]["since_id"])
+        print(since_id)
 
-    def catch_pics(self):
         # 每个json数据中有十个card
-        for i in range(10):
+        for card_num in range(10):
             try:
-                single_pic_url = self.data["data"]["cards"][i]["mblog"]["original_pic"]
+                pic_num = 1
+                for single_pic_url in data["data"]["cards"][card_num]["mblog"]["pics"]:
+                    print(single_pic_url['large']['url'])
+                    pic = get(single_pic_url['large']['url']).content
+                    with open("./%s/%s%s.jpg" % (dir_name, card_num, pic_num), "wb") as f:
+                        f.write(pic)
+                    pic_num += 1
             except KeyError:
                 continue
-            pic = get(single_pic_url).content
-            with open("./pics/%s%s.jpg" % (i, self.count), "wb") as f:
-                f.write(pic)
+
+    def search(self):
+        dir_name = self.content + 'pics_s'
+        mkdir(dir_name)
+        url = "https://m.weibo.cn/api/container/getIndex?containerid=100103" \
+              "type%3D1%26q%3D" + quote(self.content) + "&page_type=searchall"
+        r = get(url).text
+        data = loads(r)
+
+        # 每个json数据由五个card, 最后一项中存有所需数据。
+        for card_num in range(9):
+            try:
+                pic_num = 0
+                for single_pic_url in data["data"]["cards"][-1]['card_group'][card_num]["mblog"]["pics"]:
+                    print(single_pic_url['large']['url'])
+                    pic = get(single_pic_url['large']['url']).content
+                    with open("./%s/%s%s.jpg" % (dir_name, card_num, pic_num), "wb") as f:
+                        f.write(pic)
+                    pic_num += 1
+                    sleep(1)
+            except KeyError:
+                continue
 
 
 if __name__ == '__main__':
-    mkdir('pics')
     # 初始手机端主页xhr文件地址
-    url = "https://m.weibo.cn/api/container/getIndex?uid=5404464551&luicode=10000011&lfid=100103type%3D3%26q%3D%E6\
-    %9D%A8%E6%99%A8%E6%99%A8%26t%3D0&type=uid&value=5404464551&containerid=1076035404464551"
-    for num in range(2):
-        s = Spider(url, num)
-        s.catch_pics()
-        url = url + "&since_id=" + str(s.catch_since_id())
-        sleep(1)
+    s = Spider("Angelababy")
+    s.search()
